46ec63809e3a3a885412a6c4f00e2e672eb363ff
Steffi Wellington <steffisbootcampdrive@gmail.com>
2020-06-22 19:52:44 -0700

    additional components

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import Alert from "./components"
+import Alert from "./components/Alert";
+import Jumbotron from "./components/Jumbotron";
+import DeleteBtn from "./components/DeleteBtn";
+import SaveBtn from "./components/SaveBtn";
+import ViewBtn from "./components/ViewBtn";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
